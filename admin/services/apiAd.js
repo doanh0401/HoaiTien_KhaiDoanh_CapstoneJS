1346bc2814fd462bb8ee097a8b1e3a83537743af
Tien Hoai <nguyentien1668@gmail.com>
2023-07-17 21:34:10 +0700

    upload UI admin & show listproduct

---
+++ admin/services/apiAd.js
@@ -0,0 +1,9 @@
+function ProductApi() {
+    this.ListProductApi = function() {
+     var promise =  axios({
+             url: "https://64a92b9a8b9afaf4844a56e0.mockapi.io/api/CyberPhone",
+             method: "GET",
+         });
+         return promise;
+     }
+ }
