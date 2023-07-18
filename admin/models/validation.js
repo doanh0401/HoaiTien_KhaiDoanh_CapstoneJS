bd90a411f011a36d127952dce3b0d7c85ff0843f
Tien Hoai <nguyentien1668@gmail.com>
2023-07-18 22:20:37 +0700

    update validation & search product

--- admin/models/validation.js
+++ admin/models/validation.js
@@ -6,5 +6,23 @@ function Validation () {
         }
         hiddenError(errorId);
         return true;
+    };
+    this.checkEmptyOption = function (idCheck, errorId, mess) {
+        var dataCheck = document.getElementById(idCheck);
+        if (dataCheck.selectedIndex !== 0) {
+            hiddenError(errorId);
+            return true;
+        }
+        showError(errorId, mess);
+        return false;
+    };
+    this.CheckNumber = function (value, errorId, mess) {
+        var pattern = /^[0-9]+$/;
+        if (pattern.test(value)) {
+            hiddenError(errorId);
+            return true;
+        }
+        showError(errorId, mess);
+        return false;
     }
 }
