57d9174de8de92f582a29f9e82ba02131e5c9315
doanh0401 <doanhkhai.busted@gmail.com>
2023-07-25 10:19:28 +0700

    js

---
+++ customer/controller/shoppingcart.js
@@ -0,0 +1,91 @@
+var cart = new Cart();
+function setLocalStorage() {
+  var dataToString = JSON.stringify(cart.arr);
+  localStorage.setItem("Cart", dataToString);
+}
+function getLocalStorage() {
+  if (localStorage.getItem("Cart")) {
+    var dataString = localStorage.getItem("Cart");
+    var dataJSON = JSON.parse(dataString);
+    cart.arr = dataJSON;
+    //render lai Table
+  }
+}
+function getEle(id) {
+    return document.getElementById(id);
+}
+function renderCart() {
+  var content = "";
+  let total = 0;
+  let totalItem = 0;
+  if (cart.arr.length !== 0) {
+    for (var i = 0; i < cart.arr.length; i++) {
+      var cartitem = cart.arr[i];
+      content += `<tr class="table_row">
+        <td class="column-1">
+            <div class="how-itemcart1">
+                <img src="${cartitem.img}" width="70px" alt="IMG">
+            </div>
+        </td>
+        <td class="column-2">${cartitem.name}</td>
+        <td class="column-3">$ ${cartitem.price}</td>
+        <td class="column-4">
+            <div class="wrap-num-product d-flex flex-wrap ml-auto mr-0">
+                <div onclick="decrease('${cartitem.name}',${cartitem.quantity})" class="btn-num-product-down d-flex hov-btn3 flex-c-m">
+                    <i class="fa-solid fa-minus"></i>
+                </div>
+
+                <input class="mtext-104 cl3 text-center num-product" type="number"
+                    name="num-product1" value="${cartitem.quantity}">
+
+                <div onclick="increase('${cartitem.name}',${cartitem.quantity})" class="btn-num-product-up d-flex cl8 hov-btn3 flex-c-m">
+                    <i class="fa-solid fa-plus"></i>
+                </div>
+            </div>
+        </td>
+        <td onclick="XoaSP('${cartitem.name}')" style="cursor:pointer"class="column-5">Xóa</td>
+    </tr>`;
+      total += parseInt(cartitem.price) * cartitem.quantity;
+      totalItem += cartitem.quantity;
+    }
+      getEle("tableCart").innerHTML = content;
+      getEle("total").innerHTML = `$${total}`;
+      getEle("cart-item-count").innerHTML = countItem;
+  } else {
+    //   getEle("cartTable").style.display = "none";
+        getEle("total").innerHTML = `$0`;
+        getEle("cart-item-count").style.display = "none";
+  }
+}
+window.onload = function () {
+    getLocalStorage();
+    renderCart();
+  }
+function decrease(name,quantity){   
+    console.log("oke") 
+    if(quantity !== 1) quantity-=1;
+    var index = cart.timViTri(name);
+    cart.arr[index].quantity=quantity;
+    renderCart();
+    setLocalStorage();
+}
+function increase(name,quantity){
+    quantity +=1;
+    var index = cart.timViTri(name);
+    cart.arr[index].quantity=quantity;
+    SaveData();
+}
+function XoaSP(name) {
+    cart.xoaGh(name);
+    SaveData();
+}
+function SaveData() {
+    renderCart();
+    setLocalStorage();
+}
+function Pay() {
+    cart.arr= [];
+    console.log(cart.arr);
+    SaveData();
+    alert("Cám ơn bạn đã mua sắm");
+}
